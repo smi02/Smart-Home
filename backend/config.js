6283bcabbf3116fd2501151f3a54dddb35bab30c
smi02 <nguyenngocviet202@gmail.com>
2024-05-26 18:39:35 +0700

    test env

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,9 @@ export const PORT = 5555;
 export const mongoDBURL = 
 'mongodb+srv://smi02:1572881996dq@cluster0.uupxd.mongodb.net/Projects?retryWrites=true&w=majority'
 
-export const MQTT_BROKER_HOST=d17abab7fd514798be290b28e9ad3481.s1.eu.hivemq.cloud
+export const MQTT_BROKER_HOST='d17abab7fd514798be290b28e9ad3481.s1.eu.hivemq.cloud'
 export const MQTT_BROKER_PORT=8883
-export const MQTT_BROKER_PROTOCOL=mqtts
-export const MQTT_USERNAME=smi02
-export const MQTT_PASSWORD=Smilodon02
+export const MQTT_BROKER_PROTOCOL='mqtts'
+export const MQTT_USERNAME='smi02'
+export const MQTT_PASSWORD='Smilodon02'
 export const JWT_SECRET = 5254232452145
